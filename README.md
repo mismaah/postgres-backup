4dc158c8c8209fa0d4abd564be29a5a7c0115084
mismaah <mismaahabdulla@gmail.com>
2022-08-10 20:34:26 +0500

    feat: make ssh tunnel optional

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # cloudsql-backup
 
-Creates an SSH tunnel to a host with a static IP and then creates backups periodically from a GCP CloudSQL instance. The tunnel is needed so that the static IP can be whitelisted by CloudSQL.
+Creates backups periodically from a GCP CloudSQL instance. Optionally creates an SSH tunnel to a host with a static IP. The tunnel might be needed so that the static IP can be whitelisted by CloudSQL.
 
-Runs at 00:00 everyday. Change the schedule code in `script.py` to change backup intervals.
+Runs at 00:00 everyday. Change the schedule code in `script.py` line 88 to change backup intervals.
 
 ## Quickstart
 
-Rename `.env.example` to `.env` and fill all variables.
+Rename `.env.example` to `.env` and fill all variables. If SSH tunnel is not required, no need to fill in the `TUNNEL_*` variables.
 
 Build docker image.
 ```powershell
--- script.py
+++ script.py
@@ -18,24 +18,50 @@ def task():
     now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H-%M-%S")
     print(now)
     try:
-        with SSHTunnelForwarder(
-            (env("TUNNEL_HOST"), 22),
-            ssh_username=env("TUNNEL_USERNAME"),
-            ssh_password=env("TUNNEL_PASSWORD"),
-            remote_bind_address=(env("CSQL_HOST"), 5432),
-        ) as server:
+        if env("TUNNEL_HOST") != "":
+            with SSHTunnelForwarder(
+                (env("TUNNEL_HOST"), 22),
+                ssh_username=env("TUNNEL_USERNAME"),
+                ssh_password=env("TUNNEL_PASSWORD"),
+                remote_bind_address=(env("CSQL_HOST"), 5432),
+            ) as server:
 
-            server.start()
-            print("Tunnel connected")
+                server.start()
+                print("Tunnel connected")
 
+                params = {
+                    "database": env("CSQL_DATABASE"),
+                    "user": env("CSQL_USER"),
+                    "password": env("CSQL_PASSWORD"),
+                    "host": "localhost",
+                    "port": server.local_bind_port,
+                }
+
+                conn = psycopg2.connect(**params)
+                curs = conn.cursor()
+                print("Database connected")
+                curs.execute("SELECT datname FROM pg_database;")
+                dbs = curs.fetchall()
+                dbs = [i[0] for i in dbs]
+                envDbs = env("DATABASES").split(",")
+                dbs = [i for i in dbs if i in envDbs]
+
+                print("Creating new backup directory")
+                subprocess.run(f"mkdir /app/data/${now}", shell=True)
+                for db in dbs:
+                    subprocess.run(
+                        f'pg_dump -Fc --dbname=postgresql://{env("CSQL_USER")}:{env("CSQL_PASSWORD")}@localhost:{server.local_bind_port}/{db} > /app/data/${now}/{db}.dump',
+                        shell=True,
+                    )
+                    print(f"Dumped {db}")
+        else:
             params = {
                 "database": env("CSQL_DATABASE"),
                 "user": env("CSQL_USER"),
                 "password": env("CSQL_PASSWORD"),
-                "host": "localhost",
-                "port": server.local_bind_port,
+                "host": env("CSQL_HOST"),
+                "port": 5432,
             }
-
             conn = psycopg2.connect(**params)
             curs = conn.cursor()
             print("Database connected")
@@ -49,12 +75,11 @@ def task():
             subprocess.run(f"mkdir /app/data/${now}", shell=True)
             for db in dbs:
                 subprocess.run(
-                    f'pg_dump -Fc --dbname=postgresql://{env("CSQL_USER")}:{env("CSQL_PASSWORD")}@localhost:{server.local_bind_port}/{db} > /app/data/${now}/{db}.dump',
+                    f'pg_dump -Fc --dbname=postgresql://{env("CSQL_USER")}:{env("CSQL_PASSWORD")}@{env("CSQL_HOST")}:5432/{db} > /app/data/${now}/{db}.dump',
                     shell=True,
                 )
                 print(f"Dumped {db}")
-            print("Backup complete\n")
-
+        print("Backup complete\n")
     except Exception as e:
         print(e)
         print("Connection failed")

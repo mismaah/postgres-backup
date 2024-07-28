c7f148270a0d86e977919214d4507fcaa1e17c2b
mismaah <mismaahabdulla@gmail.com>
2024-07-28 15:12:58 +0500

    fix: dockerfile

--- docker.ps1
+++ docker.ps1
@@ -1,4 +1,4 @@
 docker stop cloudsql-backup
 docker rm cloudsql-backup
-docker build -f .\postgres.Dockerfile . -t cloudsql-backup
+docker build . -t cloudsql-backup
 docker run --env-file .env --name cloudsql-backup -v ${PWD}/data:/app/data cloudsql-backup
--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 
 docker stop cloudsql-backup
 docker rm cloudsql-backup
-docker build -f .\postgres.Dockerfile . -t cloudsql-backup
+docker build . -t cloudsql-backup
 docker run --env-file .env --name cloudsql-backup -v ${PWD}/data:/app/data cloudsql-backup

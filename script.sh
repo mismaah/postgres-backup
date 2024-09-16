2fe216077e1b383d449510dceafdf3b8714e04bb
mismaah <mismaahabdulla@gmail.com>
2024-09-16 13:08:20 +0500

    fix: pass arguments individually to avoid parsing errors

--- script.sh
+++ script.sh
@@ -2,7 +2,9 @@
 
 now=$(date '+%Y-%m-%dT%H-%M-%S');
 
-ALL_DBS=`psql --dbname=postgresql://$CSQL_USER:$CSQL_PASSWORD@$CSQL_HOST --tuples-only -c "SELECT datname FROM pg_database"`
+export PGPASSWORD=$CSQL_PASSWORD
+
+ALL_DBS=`psql -h $CSQL_HOST -U $CSQL_USER $CSQL_DATABASE --tuples-only -c "SELECT datname FROM pg_database"`
 
 mkdir /app/data/$now
 for db in $ALL_DBS
@@ -10,7 +12,7 @@ do
   for i in ${DATABASES//,/ }
   do
       if [ "$db" == "$i" ]; then
-        pg_dump -v --dbname=postgresql://$CSQL_USER:$CSQL_PASSWORD@$CSQL_HOST/$db | gzip -c > /app/data/$now/$db.sql.gzip
+        pg_dump -v -h $CSQL_HOST -U $CSQL_USER $db | gzip -c > /app/data/$now/$db.sql.gzip
       fi
   done
 done
